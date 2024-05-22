938e0e90d90ff92e1847f6110dd561f6bc7557e5
Muhammad Shahzaib Yaqoob <158167657+mshahzaib4@users.noreply.github.com>
2024-05-22 11:07:34 +0500

    Add files via upload

---
+++ 02 Basics of C++/01 Variables and Data Types/03/main.c
@@ -0,0 +1,31 @@
+/*Write a c program to print following shape.
+*
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    */
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    cout << "::::::::::PROGRAM1::::::::::\n\n";
+    cout << "    *\n";
+    cout << "   ***\n";
+    cout << "  *****\n";
+    cout << " *******\n";
+    cout << "*********\n";
+    cout << "*********\n";
+    cout << " *******\n";
+    cout << "  *****\n";
+    cout << "   ***\n";
+    cout << "    *\n";
+
+}
---
+++ 02 Basics of C++/01 Variables and Data Types/04/main.c
@@ -0,0 +1,20 @@
+/*7. Initialize variables as many as required and print the following.
+I have 10 rupees and you have 20 rupees.
+So we have enough money for to buy a coffee.Yahooooo....*/
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+ 
+
+    cout << "\n\n::::::::::PROGRAM7:::::::::\n";
+    cout << "::::::::::YAHOOOO::::::::::\n";
+
+    int I_M = 10, You = 20;
+    cout << "I have " << I_M << " rupes and You have " << You << " rupes.\n";
+    cout << "So we have enough money to buy a coffee. Yahoooo...";
+
+}
+
