30ba2224b3b8aeffd9b0448374938463b74a43d3
Muhammad Shahzaib Yaqoob <158167657+mshahzaib4@users.noreply.github.com>
2024-12-17 09:31:56 -0800

    Delete 03 Control Flow/01 Conditional Statements directory

--- 03 Control Flow/01 Conditional Statements/03 Elif/01.py
+++
@@ -1,6 +0,0 @@
-temp = int(input("Enter The Temperature : "))
-
-if temp > 100:
-    print("\n\nAbove the boiling point of Water.\n\n")
-else:
-    print("\n\nBelow the boiling point of Water.\n\n")
--- 03 Control Flow/01 Conditional Statements/03 Elif/02.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter Number : "))
-
-if num > 60:
-    print("Positive Number")
-
-elif num < 0:
-    print("Negative Number")
