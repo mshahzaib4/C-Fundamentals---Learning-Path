34ded97618a81b8dd948015c53b77033e421862f
Muhammad Shahzaib Yaqoob <158167657+mshahzaib4@users.noreply.github.com>
2024-12-27 10:11:53 -0800

    Add files via upload

---
+++ Hugman game/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+main()
+{
+
+    int guess=0,a=0;char lion_r, lion[4]={'l','i','o','n'};
+   cout << "=====================\n";
+   cout << "Hangman: The Game\n";
+   cout << "=====================\n";
+while(guess!=7)
+    {
+    cout<<"Guess word : ";
+    lion_r=getche();cout<<endl;
+    if(lion_r==lion[a])
+    {
+    cout<<"\nGood"<<endl;a++;
+    if(lion_r=='n')
+    break;
+    }
+    else
+        guess++;
+  if(guess==1)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==2)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==3)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<"  |   | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==4)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<" /|   | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==5)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<" /|\\  | \n";
+    cout<<"      | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==6)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<" /|\\  | \n";
+    cout<<" /    | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+  }
+  else if(guess==7)
+  {
+    cout<<"  +---+ \n";
+    cout<<"  |   | \n";
+    cout<<"  O   | \n";
+    cout<<" /|\\  | \n";
+    cout<<" / \\  | \n";
+    cout<<"      | \n";
+    cout<<" ========= \n";
+    cout<<"Game over";
+  }
+
+  }
+
+}
